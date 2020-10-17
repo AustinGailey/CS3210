be287b60636a2a4c99fb850595e193b5b02f6c17
Austin Gailey <austingailey@yahoo.com>
2020-10-17 16:20:58 -0600

    Improving Readability

--- prg02_palindromes_search/src/main/scala/PalindromesSearch.scala
+++ prg02_palindromes_search/src/main/scala/PalindromesSearch.scala
@@ -2,14 +2,15 @@
  * CS3210 - Principles of Programming Languages - Fall 2020
  * Instructor: Thyago Mota
  * Description: Prg02 - PalindromesSearch
- * Student(s) Name(s): Austin Gailey
+ * Student Name: Austin Gailey
  * 
  * Description:
- * 
  * This program takes two numbers, n and m, and generates all possible pallindromic sums
  * of n, which include m.
+ * 
  * EX:
  * n = 6, m = 2
+ * 
  * All possible palindromic sums of n:
  * (6)
  * (3, 3)
@@ -36,6 +37,7 @@ import scala.collection.mutable.ArrayBuffer
 import scala.annotation.meta.param
 
 object PalindromesSearch {
+
   val OUTPUT_FILE_NAME = "output.txt"
   var saveToFile:Boolean = false
   var count:Int = 0
@@ -52,58 +54,68 @@ object PalindromesSearch {
     * Because a palindrome is mirrored on either side, only 1/2 n permutations 
     * need to be generated.
     * The partition creates partitions for all sums (n+1)/2
+    * 
+    * Great reference for partitioning:
+    * https://stackoverflow.com/questions/10035752/elegant-python-code-for-integer-partitioning
+    * 
     * @param n
     * @param m
     */
   def createPartitions(n:Int,m:Int):Unit = {
     val k = (n+1)/2
-    for(i <- 0 until scala.math.pow(2,k-1).toInt){
+    val end = scala.math.pow(2,k-1).toInt
+    for(i <- 0 until end){
       var j:Int = i
       var count = 1
-      var part:ArrayBuffer[Int] = ArrayBuffer()
+      var partition:ArrayBuffer[Int] = ArrayBuffer()
       for(s <- 0 until k){
         if(j % 2 == 0){
-          part += count
+          partition += count
           count = 1
         }else{
           count += 1
         }
         j >>= 1
       }
-      if(part != ArrayBuffer.empty) addPalindromeByPartition(part.toArray,n, m)
+      if(partition != ArrayBuffer.empty) addPalindromeByPartition(partition.toArray,n, m)
     }
   }
 
   /**
-    * Generates palindromes based on User parameters
+    * Generates palindromes based on partition
+    * 
     * @param partition
     * @param n
     * @param m
     */
   def addPalindromeByPartition(partition:Array[Int],n:Int, m:Int){
     if(n % 2 == 1){
-        var palindrome:Array[Int] = (partition.slice(0,partition.length-1)
-                         ++(Array[Int](partition(partition.length-1)*2-1))
-                         ++partition.slice(0,partition.length-1).reverse)
-        if(palindrome.contains(m)){
-          count+= 1
-          print(palindrome.mkString(",").toString)
-        }
-      }else{
-        var palindrome = (partition.slice(0,partition.length-1)
-                         ++(Array[Int](partition(partition.length-1)*2))
-                         ++partition.slice(0,partition.length-1).reverse)
-        if(palindrome.contains(m)){
-          count+= 1
-          print(palindrome.mkString(",").toString)
-        }
-        if(partition.contains(m)){
-          palindrome = (partition.slice(0,partition.length)
-                         ++partition.slice(0,partition.length).reverse)
-          count += 1
-          print(palindrome.mkString(",").toString)
-        }
-      }
+      var palindrome:Array[Int] = (partition.slice(0,partition.length-1)
+        ++(Array[Int](partition(partition.length-1)*2-1))
+        ++partition.slice(0,partition.length-1).reverse)
+      checkPalindrome(palindrome, m)
+    }else{
+      var palindrome = (partition.slice(0,partition.length-1)
+        ++(Array[Int](partition(partition.length-1)*2))
+        ++partition.slice(0,partition.length-1).reverse)
+      checkPalindrome(palindrome, m)
+      palindrome = (partition.slice(0,partition.length)
+        ++partition.slice(0,partition.length).reverse)
+      checkPalindrome(palindrome, m)
+    }
+  }
+
+  /**
+    * Check if palindrome contains m
+    *
+    * @param palindrome
+    * @param m
+    */
+  def checkPalindrome(palindrome:Array[Int], m:Int){
+    if(palindrome.contains(m)){
+      count+= 1
+      print(palindrome.mkString(",").toString)
+    }
   }
 
   /**
@@ -129,7 +141,7 @@ object PalindromesSearch {
   }
 
   /**
-    *Calculates time to run project and prints finishing messages
+    * Calculates time to run project and prints finishing messages
     * @param time
     */
   def finishProject(time:Long):Unit = {
@@ -142,7 +154,7 @@ object PalindromesSearch {
   }
 
   /**
-    * Appends to output file
+    * Appends line to output file
     *
     * @param line
     */
