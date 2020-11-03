3556df3126bae1dcda9b43021e66eaf6d169be82
Austin Gailey <austingailey@yahoo.com>
2020-11-02 17:17:26 -0700

    Adding hwk_09

--- hwk_09_prolog/hwk09.pl
+++ hwk_09_prolog/hwk09.pl
@@ -1,38 +1,54 @@
-mountain('Mt. Everest','Asia',29029).
-mountain('Aconcagua','South America',22841).
-mountain('Mt. McKinley','North America',20312).
-mountain('Kilimanjaro','Africa',19340).
-mountain('Mt. Elbrus','Europe',18510).
-mountain('Mt. Vinson','Antartica',16050).
-mountain('Puncak Jaya','Australia',16023).
+% Facts about names
+peak('Mt. Everest').
+peak('The Aconcagua').
+peak('Mt. McKinley').
+peak('The Kilimanjaro').
+peak('Mt. Elbrus').
+peak('Mt. Vinson').
+peak('The Puncak Jaya').
+
+% Facts about locations
+location('Mt. Everest', 'Asia')             :- peak('Mt. Everest').
+location('The Aconcagua','South America')   :- peak('The Aconcagua').
+location('Mt. McKinley', 'North America')   :- peak('Mt. McKinley').
+location('The Kilimanjaro', 'Africa')       :- peak('The Kilimanjaro').
+location('Mt. Elbrus', 'Europe')            :- peak('Mt. Elbrus').
+location('Mt. Vinson', 'Antartica')         :- peak('Mt. Vinson').
+location('The Puncak Jaya', 'Australia')    :- peak('The Puncak Jaya').
+
+% Facts about elevations
+elevation('Mt. Everest', 29029)             :- peak('Mt. Everest').
+elevation('The Aconcagua', 22841)           :- peak('The Aconcagua').
+elevation('Mt. McKinley', 22841)            :- peak('Mt. McKinley').
+elevation('The Kilimanjaro', 19340)         :- peak('The Kilimanjaro').
+elevation('Mt. Elbrus', 18510)              :- peak('Mt. Elbrus').
+elevation('Mt. Vinson', 16050)              :- peak('Mt. Vinson').
+elevation('The Puncak Jaya', 16023)         :- peak('The Puncak Jaya').
+
+% Facts about climbers
 climber('John').
 climber('Kelly').
 climber('Maria').
 climber('Derek').
 climber('Thyago').
-certified('John').
-certified('Kelly').
-certified('Maria').
-certified('Derek').
-would_climb('John') :-
+
+% Facts about certifications
+certified('John')   :- climber('John').
+certified('Kelly')  :- climber('Kelly').
+certified('Maria')  :- climber('Maria').
+certified('Derek')  :- climber('Derek').
+
+% Rules about which mountains climbers will climb
+would_climb('John', M) :-
     certified('John'),
-    mountain(Name,Continent,_),
-    Continent == 'North America',
-    write(Name).  
-would_climb('Kelly') :-
+    location(M,'North America').
+would_climb('Kelly', M) :-
     certified('Kelly'),
-    mountain(Name,_,Elevation),
-    Elevation>=20000,
-    write(Name).
-would_climb('Maria') :-
+    elevation(M, E),E>=20000.
+would_climb('Maria', M) :-  
     certified('Maria'),
-    mountain(Name,_,_),
-    write(Name).
-would_climb('Derek') :-
+    peak(M).
+would_climb('Derek', M) :-
     certified('Derek'),
-    mountain(Name,Continent,Elevation),
-    (Continent == 'Europe'
-    ;Continent == 'Africa'
-    ;Continent == 'Australia'),
-    Elevation=<19000,
-    write(Name).
+    (location(M,'Europe') ; location(M,'Africa') ; location(M,'Australia')),
+    elevation(M,E),E=<19000.
